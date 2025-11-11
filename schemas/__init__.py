8fbcb0b06fc2f601ed9e3a7f4743f8723ae3e1dc
Banan41k78 <dan78810513@gmail.com>
2025-11-11 21:41:00 +0300

    Delete schemas directory

--- schemas/__init__.py
+++
@@ -1,8 +0,0 @@
-"""
-Pydantic schemas for request/response validation
-"""
-
-from app.schemas.requests import PredictionRequest
-from app.schemas.responses import PredictionResponse
-
-__all__ = ["PredictionRequest", "PredictionResponse"]
